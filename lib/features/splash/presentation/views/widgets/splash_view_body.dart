297da6e719dd9530284f58faea9c32145ece9479
= <tarekmohammed022@gmail.com>
2025-06-22 03:47:15 +0300

    add animation to the splash body view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,28 +1,51 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 4), end: Offset.zero)
+            .animate(animationController);
+
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+    animationController.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: SizedBox(
-            height: 100,
-            width: 150,
-            child: Image.asset(Assets.logo),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(Assets.logo),
+          const SizedBox(height: 10),
+          SlideTransition(
+            position: slidingAnimation,
+            child: const Text(
+              "Red Free Books",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
           ),
-        ),
-        const Center(
-          child: Text(
-            "Red Free Books ",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
